3a694beaa9af851995871adc33c94d1cf47d16ef
gdribeiro <giovaned.ribeiro@gmail.com>
2016-01-10 22:01:06 +0200

    Just Another commit

---
+++ functions-in-py.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+# Functions examples file
+
+# They say the name of the first line of a Function is "header"
+# And the rest is called "body"
+def  print_words():
+    print 'Once upon a time'
+    print 'There was a python script that'
+    print 'ended in .py'
+    print 'But if I want to use an apostrophe in a print'
+    print "I have to print it with doble quotes"
+    print "like here where are say that I'll print an '"
+
+    pass
+
+def do_the_printing_again():
+    print_words()
+    pass
+
+# just calling the function
+print_words()
+# Calling a funciont from another function
+do_the_printing_again()
+
+
+# So far so stupid...
